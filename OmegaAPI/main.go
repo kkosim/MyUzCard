aaba95bfb1bcbde7d996d305c533153b57d76e38
Qosim Qosimi <k.kosimi@yandex.com>
2024-03-11 10:38:33 +0500

    first stage

---
+++ OmegaAPI/main.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"MyUzcardTransfer/models"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var token string
+
+func main() {
+	rOmega := gin.Default()
+
+	rOmega.POST("/api/Authorization/login", func(c *gin.Context) {
+		var loginData models.RequestLogin
+		if err := c.ShouldBindJSON(&loginData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Auth logic
+		if loginData.Username == "test" && loginData.Password == "test@123" {
+			token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNiIsImp0aSI6IjlhYjQ2NzJhLTE3ZWYtNGNkYy04MTc4LWE1NjNiMWFjNzEzMSIsImlhdCI6IjYvNi8yMDIyIDU6MTE6MDQgQU0iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93c"
+
+			c.JSON(http.StatusOK, models.Response{
+				Results: &models.Results{
+					ResultLogin: &models.ResultLogin{
+						Token:      token,
+						UserID:     26,
+						ExpireDate: "2022-06-06T10:20:59.1529539",
+						RoleID:     2,
+					},
+				},
+				Error:   nil,
+				Success: true,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, models.Response{
+				Results: nil,
+				Error: &models.Error{
+					Code:    -2,
+					Message: "User not found",
+				},
+				Success: false,
+			})
+		}
+	})
+
+	rOmega.POST("/api/Authorization/check", func(c *gin.Context) {
+		var req models.RequestCheck
+		err := c.ShouldBindJSON(&req)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		header := c.GetHeader("Authorization")
+		headerParts := strings.Split(header, " ")
+		if headerParts[1] == token {
+			c.JSON(http.StatusOK, models.Response{
+				Results: &models.Results{
+					ResultCheck: &models.ResultCheck{
+						TransactionNumber: "11112222070580de84dbf6-8183-412c-9ba8-06abb4f96c6c",
+						State:             0,
+						CreatedDate:       "2022-11-11T22:07:06.1931708",
+						Amount:            1000,
+						CommissionAmount:  2,
+						TotalAmount:       1002,
+						CardHolder:        "MIRKURBONOV MIRGANI GULO",
+						BankName:          "IpakYuliBank",
+						CurrencyInfo: &models.CurrencyInfo{
+							From: "USD",
+							To:   "UZS",
+							Rate: 11306.79,
+						},
+					},
+				},
+				Error:   nil,
+				Success: true,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, models.Response{
+				Results: nil,
+				Error: &models.Error{
+					Code:    -100,
+					Message: "internal server error",
+				},
+				Success: false,
+			})
+		}
+	})
+
+	rOmega.POST("/api/Authorization/pay", func(c *gin.Context) {
+		var req models.TransactionNumber
+		err := c.ShouldBindJSON(&req)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		header := c.GetHeader("Authorization")
+		headerParts := strings.Split(header, " ")
+		if headerParts[1] == token {
+			c.JSON(http.StatusOK, models.Response{
+				Results: &models.Results{
+					ResultPay: &models.ResultPay{
+						TransactionNumber: "cc6895851c10b9386c2418008b02af39",
+						State:             1,
+						CreatedDate:       "28.10.2020 10:00:00",
+						PaymentDate:       "28.10.2020 10:01:58",
+						Amount:            12.3,
+						TotalAmount:       12,
+						CommissionAmount:  1,
+					},
+				},
+				Error:   nil,
+				Success: true,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, models.Response{
+				Results: nil,
+				Error: &models.Error{
+					Code:    -100,
+					Message: "internal server error",
+				},
+				Success: false,
+			})
+		}
+	})
+
+	rOmega.POST("/api/Authorization/get-status", func(c *gin.Context) {
+		var req models.TransactionNumber
+		err := c.ShouldBindJSON(&req)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		header := c.GetHeader("Authorization")
+		headerParts := strings.Split(header, " ")
+		if headerParts[1] == token {
+			c.JSON(http.StatusOK, models.Response{
+				Results: &models.Results{
+					ResultGetStatus: &models.ResultGetStatus{
+						State:            1,
+						CreatedDate:      "28.10.2020 10:00:00",
+						PaymentDate:      "28.10.2020 10:01:58",
+						Amount:           12.3,
+						TotalAmount:      12,
+						CommissionAmount: 1,
+					},
+				},
+				Error:   nil,
+				Success: true,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, models.Response{
+				Results: nil,
+				Error: &models.Error{
+					Code:    -100,
+					Message: "internal server error",
+				},
+				Success: false,
+			})
+		}
+	})
+
+	rOmega.POST("/api/Authorization/reverse", func(c *gin.Context) {
+		var req models.TransactionNumber
+		err := c.ShouldBindJSON(&req)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		header := c.GetHeader("Authorization")
+		headerParts := strings.Split(header, " ")
+		if headerParts[1] == token {
+			c.JSON(http.StatusOK, models.Response{
+				Results: &models.Results{
+					ResultReverse: &models.ResultReverse{
+						State:            1,
+						CreatedDate:      "28.10.2020 10:00:00",
+						PaymentDate:      "28.10.2020 10:01:58",
+						Amount:           12.3,
+						TotalAmount:      12,
+						CommissionAmount: 1,
+						ReverseDate:      time.Now(),
+					},
+				},
+				Error:   nil,
+				Success: true,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, models.Response{
+				Results: nil,
+				Error: &models.Error{
+					Code:    -100,
+					Message: "internal server error",
+				},
+				Success: false,
+			})
+		}
+	})
+
+	rOmega.POST("/api/Authorization/p2pinfowrap", func(c *gin.Context) {
+		var req models.CardNumber
+		err := c.ShouldBindJSON(&req)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		header := c.GetHeader("Authorization")
+		headerParts := strings.Split(header, " ")
+		if headerParts[1] == token {
+			c.JSON(http.StatusOK, models.Response{
+				Results: &models.Results{
+					ResultP2pinfowrap: &models.ResultP2pinfowrap{
+						CardHolder: "Mirgani Mirkurbonov",
+						BankName:   "Ipak Yuli Bank",
+					},
+				},
+				Error:   nil,
+				Success: true,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, models.Response{
+				Results: nil,
+				Error: &models.Error{
+					Code:    -100,
+					Message: "internal server error",
+				},
+				Success: false,
+			})
+		}
+	})
+
+	log.Println("Starting server on port 8081...")
+	err := rOmega.Run(":8081")
+	if err != nil {
+		log.Println("Error while running a server")
+		return
+	}
+}
---
+++ models/models.go
@@ -0,0 +1,129 @@
+package models
+
+import "time"
+
+// RequestLogin структура для хранения логина и пароля
+type RequestLogin struct {
+	Username string `json:"userName"`
+	Password string `json:"password"`
+}
+
+// Response структура для хранения ответа от Узбекистана
+type Response struct {
+	Results *Results `json:"result"`
+	Error   *Error   `json:"error"`
+	Success bool     `json:"success"`
+}
+
+// Error структура храненния вывода ошибок
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// Results структура хранения всех Result
+type Results struct {
+	ResultLogin       *ResultLogin       `json:"resultLogin,omitempty"`
+	ResultCheck       *ResultCheck       `json:"resultCheck,omitempty"`
+	ResultPay         *ResultPay         `json:"resultPay,omitempty"`
+	ResultGetStatus   *ResultGetStatus   `json:"resultGetStatus,omitempty"`
+	ResultReverse     *ResultReverse     `json:"resultReverse,omitempty"`
+	ResultP2pinfowrap *ResultP2pinfowrap `json:"resultP2Pinfowrap,omitempty"`
+}
+
+// ResultLogin структура хранения содержания ответа на /login
+type ResultLogin struct {
+	Token      string `json:"token"`
+	UserID     int64  `json:"userId"`
+	ExpireDate string `json:"expireDate"`
+	RoleID     int64  `json:"roleId"`
+}
+
+type CurrencyInfo struct {
+	From string  `json:"from"`
+	To   string  `json:"to"`
+	Rate float64 `json:"rate"`
+}
+
+type ResultCheck struct {
+	TransactionNumber string        `json:"transactionNumber"`
+	State             int           `json:"state"`
+	CreatedDate       string        `json:"createdDate"`
+	Amount            int           `json:"amount"`
+	CommissionAmount  int           `json:"commissionAmount"`
+	TotalAmount       int           `json:"totalAmount"`
+	CardHolder        string        `json:"cardHolder"`
+	BankName          string        `json:"bankName"`
+	CurrencyInfo      *CurrencyInfo `json:"currencyInfo"`
+}
+
+type SenderDocument struct {
+	Nationality string `json:"nationality"`
+	DocType     string `json:"docType"`
+	DocSerial   string `json:"docSerial"`
+	DocNumber   string `json:"docNumber"`
+	ValidTo     string `json:"validTo"`
+	BirthDate   string `json:"birthDate"`
+}
+
+type Sender struct {
+	CardNumber     string          `json:"cardNumber"`
+	CardIssuer     string          `json:"cardIssuer"`
+	Country        string          `json:"country"`
+	SenderDocument *SenderDocument `json:"senderDocument"`
+}
+
+type RequestCheck struct {
+	Amount                   float64 `json:"amount"`
+	CardNumber               string  `json:"cardNumber"`
+	Currency                 string  `json:"currency"`
+	PartnerClientId          string  `json:"partnerClientId"`
+	PartnerTransactionNumber string  `json:"partnerTransactionNumber"`
+	SenderFirstName          string  `json:"senderFirstName"`
+	SenderMiddleName         *string `json:"senderMiddleName"`
+	SenderLastName           string  `json:"senderLastName"`
+	SenderAddress            *string `json:"senderAddress"`
+	Sender                   *Sender `json:"sender"`
+}
+
+type ResultPay struct {
+	TransactionNumber string  `json:"transactionNumber"`
+	State             int     `json:"state"`
+	CreatedDate       string  `json:"createdDate"`
+	PaymentDate       string  `json:"paymentDate"`
+	Amount            float64 `json:"amount"`
+	TotalAmount       float64 `json:"totalAmount"`
+	CommissionAmount  float64 `json:"commissionAmount"`
+}
+
+type TransactionNumber struct {
+	TransactionNumber string `json:"transactionNumber"`
+}
+
+type ResultGetStatus struct {
+	State            int     `json:"state"`
+	CreatedDate      string  `json:"createdDate"`
+	PaymentDate      string  `json:"paymentDate"`
+	Amount           float64 `json:"amount"`
+	TotalAmount      float64 `json:"totalAmount"`
+	CommissionAmount float64 `json:"commissionAmount"`
+}
+
+type ResultReverse struct {
+	State            int       `json:"state"`
+	CreatedDate      string    `json:"createdDate"`
+	PaymentDate      string    `json:"paymentDate"`
+	Amount           float64   `json:"amount"`
+	TotalAmount      float64   `json:"totalAmount"`
+	CommissionAmount float64   `json:"commissionAmount"`
+	ReverseDate      time.Time `json:"reverseDate"`
+}
+
+type CardNumber struct {
+	CardNumber string `json:"cardNumber"`
+}
+
+type ResultP2pinfowrap struct {
+	CardHolder string `json:"cardHolder"`
+	BankName   string `json:"bankName"`
+}
